3e6b0a6cedd90163cd1a20b3c11d03bd5bc2626c
Priyanshu <Priyanshukaushal878@gmail.com>
2021-01-06 20:04:55 +0530

    Added card12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Logo from './Logo.js';
+import Card from './Components/Card/card';
 function App() {
   return (
     <div>
       <Logo></Logo>
+      <Card></Card>
     </div>
   );
 }
