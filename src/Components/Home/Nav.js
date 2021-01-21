4dc7be04dba09324225cfc0f5b6f014c46741e71
Sanjay Prajapati <sanjay270899@gmail.com>
2021-01-22 01:43:00 +0530

    Mobile Nav-Bar Array

--- src/Components/Home/Nav.js
+++ src/Components/Home/Nav.js
@@ -29,7 +29,7 @@ export default function Nav() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center  py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="/">
               <span className="sr-only">Workflow</span>
               <Logo />
             </a>
@@ -43,8 +43,8 @@ export default function Nav() {
               ))
             }
             <a
-              href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-full shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 shadow-xl "
+              href="/"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-full text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 shadow-xl"
             >
               Share Your idea
             </a>
@@ -86,36 +86,15 @@ export default function Nav() {
             navButton && 
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                {
+                  navLinks.map(element => (
+                    <a href={element.to} className="w-full flex items-center justify-center px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700" key={element.name}>
+                      {element.name}
+                    </a>
+                  ))
+                }
                 <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Home
-                </a>
-
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Join
-                </a>
-
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  About
-                </a>
-
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Help
-                </a>
-
-                <a
-                  href="#"
+                  href="/"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Share your idea
