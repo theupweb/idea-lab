4a697a841a505099f81950b976e1aafb8eade7c8
Sanjay Prajapati <sanjay270899@gmail.com>
2021-02-01 03:03:47 +0530

    Supabase Setup and Authentication

---
+++ src/supabase/initSupabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);
